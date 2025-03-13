4cfb182ff1a9fd160d458fd591716ec37e98aa51
Farah <frhmph.2807@gmail.com>
2025-03-13 13:07:36 +0700

    alert

--- resources/js/pages/register.vue
+++ resources/js/pages/register.vue
@@ -4,6 +4,7 @@ import GuestLayout from "../components/GuestLayout.vue";
 import axiosClient from "../axios";
 import router from "../router";
 import { RouterLink } from "vue-router";
+import Swal from "sweetalert2"; // Import SweetAlert2
 
 const data = ref({
     name: "",
@@ -26,6 +27,12 @@ const submitRegister = async () => {
             localStorage.setItem("token", response.data.token);
             axiosClient.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
         }
+        Swal.fire({
+            icon: 'success',
+            title: 'Registasi Berhasil!'
+            // timer: 2000, // Auto-close dalam 2 detik
+            // showConfirmButton: false
+        });
 
         // Redirect ke dashboard atau halaman utama setelah registrasi sukses
         router.push("/");
@@ -33,6 +40,12 @@ const submitRegister = async () => {
         if (error.response) {
             errorMessage.value = error.response.data.message || "Registrasi gagal. Coba lagi!";
             // alert("Email sudah terdaftar")
+            Swal.fire({
+                icon: 'error',
+                title: 'Email sudah terdaftar!'
+                // timer: 2000, // Auto-close dalam 2 detik
+                // showConfirmButton: false
+            });
         } else {
             errorMessage.value = "Terjadi kesalahan. Silakan coba lagi.";
         }
@@ -49,10 +62,10 @@ const submitRegister = async () => {
             <form @submit.prevent="submitRegister" class="space-y-6 ml-7 mt-3">
                 <div>
                     <div class="flex items-center justify-between">
-                    <label for="name" class="block text-sm/ font-medium text-gray-900 mt-4">Nama</label>
-                    <input type="name" name="name" id="name" autocomplete="name" required="" v-model="data.name"
-                        class="mt-3 mr-5 block w-60 rounded-md bg-white px-3 py-1.5 focus:outline-black ">
-                        </div>
+                        <label for="name" class="block text-sm/ font-medium text-gray-900 mt-4">Nama</label>
+                        <input type="name" name="name" id="name" autocomplete="name" required="" v-model="data.name"
+                            class="mt-3 mr-5 block w-60 rounded-md bg-white px-3 py-1.5 focus:outline-black ">
+                    </div>
                     <!-- <div class="mt-0.5">
                     </div> -->
                 </div>
@@ -60,7 +73,8 @@ const submitRegister = async () => {
                     <div class="flex items-center justify-between">
                         <label for="email" class="block text-sm/ font-medium text-gray-900 mt-3">Email</label>
                         <input type="email" name="email" id="email" autocomplete="email" required=""
-                            v-model="data.email" class="mt-2 mr-5 block w-60 rounded-md bg-white px-3 py-1.5 focus:outline-black">
+                            v-model="data.email"
+                            class="mt-2 mr-5 block w-60 rounded-md bg-white px-3 py-1.5 focus:outline-black">
                     </div>
                     <!-- <div class="mt-0.5">
                         <input type="email" name="email" id="email" autocomplete="email" required=""
@@ -90,16 +104,22 @@ const submitRegister = async () => {
                     <a id="kembali" href="/">Kembali</a>
                 </RouterLink> -->
             </form>
-
+            <p class="font-semibold  text-center text-sm/6 text-black" id="tes">
+                Sudah Memiliki Akun?
+                <!-- {{ " " }} -->
+                <!-- <a href="/register" class="font-semibold" id="buat">Buat Akun</a> -->
+                <router-Link to="/" class="font-semibold" id="buat">Masuk</router-Link>
+            </p>
         </div>
     </GuestLayout>
 </template>
 
 <style>
-#kembali{
+#kembali {
     color: aliceblue;
 }
-#kembali:hover{
+
+#kembali:hover {
     color: rgb(0, 0, 0);
 }
 </style>
